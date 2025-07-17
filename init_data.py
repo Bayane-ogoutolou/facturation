743160db8897a0cf5ca26059825b758585cf5366
Abdoulkader MOHAMED SALEM <abfoulmed645@gmail.com>
2025-07-17 19:45:51 +0100

    Recuperation des donnéés depuis les fichier Excels"

--- init_data.py
+++ init_data.py
@@ -1,1 +1,10 @@
+import pandas as pd
+
+contenu1 = pd.read_excel("../Clients.xlsx")
+contenu2 = pd.read_excel("../Produits.xlsx")
+
+clients = pd.DataFrame(contenu1)
+produits = pd.DataFrame(contenu2)
+
+print(clients,"\n\n",produits)
 
